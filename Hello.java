e0d3dbfb82ccd857eace473c1811cb4f04e3086e
ramyamaddala06 <ramyamaddala216@gmail.com>
2024-03-17 13:21:25 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,189 @@
+/*
+ * class Hello
+ * {
+ * public static void main(String[] args) {
+ * System.out.println("Hello Ramya");
+ * }
+ * }
+ */
+
+/*
+ * class Hello {
+ * public static void main(String[] args) {
+ * System.out.println(21 - 6);
+ * System.out.println(24 - 10);
+ * }
+ * }
+ */
+
+/*
+ * class Hello {
+ * public static void main(String[] args) {
+ * int num1 = 18;
+ * int num2 = 6;
+ * int result = num1 + num2;
+ * System.out.println(result);
+ * }
+ * }
+ */
+
+/*
+ * class Hello {
+ * public static void main(String args[]) {
+ * int num1 = 30;
+ * System.out.println(num1);
+ * }
+ * }
+ */
+
+/*
+ * class Hello {
+ * public static void main(String args[]) {
+ * char c = 'r';
+ * c++;
+ * System.out.println(c);
+ * }
+ * }
+ */
+
+/*class Hello {
+  public static void main(String args[]) {
+    byte b = 125;
+    int a = b;
+    System.out.println(a);
+  }
+}*/
+
+/*class Hello {
+  public static void main(String args[]) {
+    int a = 257;
+    byte b = (byte) a;
+    System.out.println(b);
+  }
+}*/
+/*class Hello {
+  public static void main(String args[]) {
+    int a = 18;
+    float b = (float) a;
+    System.out.println(a);
+  }
+}*/
+/*class Hello {
+  public static void main(String args[]) {
+    float f = 1.8f;
+    int k = (int) f;
+    System.out.println(k);
+
+  }
+}*/
+
+/*class Hello {
+  public static void main(String args[]) {
+    byte a = 18;
+    byte b = 6;
+    int result = a * b;
+    System.out.println(result);
+  }
+}*/
+
+/*class Hello {
+  public static void main(String args[]) {
+    int num1 = 24;
+    int num2 = 10;
+    int result = num1 + num2;
+    System.out.println(result);
+  }
+}*/
+
+/*class Hello {
+  public static void main(String args[]) {
+    int num1 = 24;
+    int num2 = 10;
+    int result = num1 - num2;
+    System.out.println(result);
+  }
+}*/
+
+/*class Hello {
+  public static void main(String args[]) {
+    int num1 = 24;
+    int num2 = 10;
+    int result = num1 * num2;
+    System.out.println(result);
+  }
+}*/
+
+/*class Hello {
+  public static void main(String args[]) {
+    int num1 = 24;
+    int num2 = 10;
+    int result = num1 / num2;
+    System.out.println(result);
+  }
+}*/
+
+/*class Demo {
+  public static void main(String args[]) {
+    int x = 6;
+    int y = 4;
+    boolean result = x < y;
+    System.out.println(result);
+  }
+}*/
+
+/*class Hello {
+  public static void main(String args[]) {
+    byte b = 127;
+    int a = b;
+    System.out.println(a);
+  }
+}*/
+
+/*class Hello {
+  public static void main(String args[]) {
+    int x = 18;
+    int y = 6;
+    boolean result = x < y;
+    System.out.println(result);
+  }
+}*/
+
+/*class Hello {
+  public static void main(String args[]) {
+    int a = 24;
+    int b = 10;
+    boolean result = a > b;
+    System.out.println(result);
+  }
+}*/
+
+/*class Hello {
+  public static void main(String args[]) {
+    int num1 = 30;
+    ++num1;
+    System.out.println(num1);
+
+  }
+}*/
+
+/*class Hello {
+  public static void main(String args[]) {
+    int x = 10;
+    int y = 24;
+    int a = 21;
+    int b = 6;
+    boolean result = x < y && a > b;
+    System.out.println(result);
+  }
+}*/
+
+class Hello {
+  public static void main(String args[]) {
+    int x = 24;
+    int y = 10;
+    int a = 6;
+    int b = 21;
+    boolean result = x < y || a > b;
+    System.out.println(!result);
+  }
+}
