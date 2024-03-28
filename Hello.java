66fbec0a228e627a43adf8600d481991a38b7564
ramyamaddala06 <ramyamaddala216@gmail.com>
2024-03-28 21:03:58 +0530

    array

--- Hello.java
+++ Hello.java
@@ -726,3 +726,138 @@ public class Hello {
 
   }
 }*/
+
+/*public class Hello {
+	public static void main(String[] args) {
+//      int nums[]= {3,7,2,4};
+//		nums[1]=6;
+//		System.out.println(nums[1]);
+		
+		int nums[]=new int[4];
+		nums[0]=4;
+		nums[1]=8;
+		nums[2]=3;
+		nums[3]=9;
+		
+//		System.out.println(nums[0]);
+//		System.out.println(nums[1]);
+//		System.out.println(nums[2]);
+//		System.out.println(nums[3]);
+		
+		for (int i=0;i<4;i++) {
+			System.out.println(nums[i]);
+		}
+		
+	}
+}*/
+
+/*public class Hello {
+	public static void main(String[] args) {
+		int nums[][]=new int [3][4];
+		//inr random=0;
+		//int random=(int)Math.random()*100;
+		
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<4;j++)
+			{
+				nums[i][j]=(int)Math.random()*100;
+				System.out.println(nums[i][j]);
+			}
+			System.out.println();
+		}
+		
+		
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<4;j++)
+			{
+				System.out.println(nums[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+		for(int n[]:nums)
+		{
+			for(int m:n)
+			{
+				System.out.println(m+" ");
+			}
+			System.out.println();
+		}
+	}
+}*/
+
+/*public class Hello {
+	public static void main(String[] args) 
+	{
+//		int nums[][]=new int [3][]; 
+//		nums[0]=new int [3];
+//		nums[1]=new int [4];
+//		nums[2]=new int [2];
+		
+		int nums[][]=new int [3][4];
+//		int nums[][]=new int [3][4][5]; 
+		
+		for(int i=0;i<nums.length;i++)
+		{
+			for(int j=0;j<nums[i].length;j++)
+			{
+				nums[i][j]=(int)(Math.random()*10);
+			}
+		}
+	}
+}*/
+
+
+class Student
+{
+	int rollno;
+	String name;
+	int marks;	
+}
+
+public class Hello {
+	public static void main(String[] args) 
+	{
+		Student s1=new Student();
+		s1.rollno=1;
+		s1.name="Ramya";
+		s1.marks=88;
+		
+		Student s2=new Student();
+		s2.rollno=2;
+		s2.name="Sowmya";
+		s2.marks=67;
+		
+		Student s3=new Student();
+		s3.rollno=3;
+		s3.name="";
+		s3.marks=97;
+		
+		System.out.println(s1.name + ":"+ s1.marks);
+		
+		Student students[]=new Student[3];
+		students[0]=s1;
+		students[1]=s2;
+		students[2]=s3;
+		
+		for(int i=0;i<students.length;i++)
+		{
+			System.out.println(students[i].name+":"+students[i].marks);
+		}
+		
+		
+		
+//		int nums[] = new int[6];
+//		nums[0]=4;
+//		nums[1]=8;
+//		nums[2]=3;
+//		nums[3]=9;
+//		
+//		for(int i=0;i<nums.length;i++)
+//		{
+//			System.out.println(nums[i]);
+//		}
+	}
+}
