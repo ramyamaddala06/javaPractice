e69976ddcce0cc69f8c4c2334b2fcd444ced316f
ramyamaddala06 <ramyamaddala216@gmail.com>
2024-04-05 19:06:45 +0530

    ReverseString

--- Hello.java
+++ Hello.java
@@ -988,3 +988,23 @@ public class Hello {
 	}
 }*/
 
+ /*class Hello {
+  public static void main(String args[]){
+    String original="Hello World";
+    String reversed=reverseString(original);
+    System.out.println("Original string: " + original);     //reverse string
+    System.out.println("Reversed string: " + reversed);
+  }
+public static String reverseString(String str) {
+  StringBuilder sb=new StringBuilder(str);
+  sb.reverse();
+  return sb.toString();
+}
+
+ }*/
+ 
+
+
+
+
+
