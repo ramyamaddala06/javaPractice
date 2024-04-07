58c72ca781f300dd28da6ea9c8e912c61163a23c
ramyamaddala06 <ramyamaddala216@gmail.com>
2024-04-07 10:35:29 +0530

    swapping

--- Hello.java
+++ Hello.java
@@ -1002,7 +1002,18 @@ public static String reverseString(String str) {
 }
 
  }*/
- 
+
+ public class Hello{
+  public static void main(String args[]){
+    int a=10,b=20;
+    System.out.println("Before swap: a = "+ a +",b="+b);
+    int temp=a;
+    a=b;
+    b=temp;
+    System.out.println("After swap: a = "+ a + ",b="+b);
+  }
+ }
+
 
 
 
